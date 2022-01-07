5163e6643e210c198ca361dc6acf04f9a5bcddde
adrian-commit <santillanadrianalberto@hotmail.com>
2022-01-06 21:43:11 -0300

    carga de estructura de archivos node*express sprint 2

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("Alfajores Big BROS")
+const express = require('express');
+const app = express();
+
+
+app.use('/static', express.static(__dirname + '/public'));
+
+app.set('port', process.env.PORT || 3000) 
+
+app.listen(app.get('port'), ()=> console.log('On http://localhost:'+app.get('port')));
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/productCar', (req,res)=>{
+    res.sendFile(__dirname + '/views/productCar.html');
+});
+
+app.get('/productDetail', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
+
+app.get('/register', (req,res)=>{   
+    res.sendFile(__dirname + '/views/register.html');
+});
