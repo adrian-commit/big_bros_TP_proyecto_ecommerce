eaf29f07216710427a79096d2c02bee088d87dbc
noegal97 <noeliagale97@gmail.com>
2022-01-10 23:10:49 -0300

    coordinando con Adri

--- app.js
+++ app.js
@@ -29,10 +29,11 @@ app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
 });
 
-app.get('/panelUsuario', (req,res)=>{   
+app.get('/miCuenta', (req,res)=>{   
     res.sendFile(__dirname + '/views/panelUsuario.html');
 });
 
 app.get('/nosotros', (req,res)=>{   
     res.sendFile(__dirname + '/views/nosotros.html');
 });
+
