88f4679d09b032fb52f4614b71c5b0d9a72b6884
noegal97 <noeliagale97@gmail.com>
2022-01-29 17:37:58 -0300

    Cambiando rutas a path join

--- app.js
+++ app.js
@@ -10,30 +10,29 @@ app.set('port', process.env.PORT || 3000)
 
 app.listen(app.get('port'), ()=> console.log('On http://localhost:'+app.get('port')));
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+app.get("/",(req,res)=> {
+    let home = path.join(__dirname + '/views/home.html');
+    res.sendFile(home);
+})
+
+
+app.get("/miCuenta",(req,res)=> {
+    let miCuenta = path.join(__dirname + '/views/panelUsuario.html');
+    res.sendFile(miCuenta);
 });
 
-app.get('/productCar', (req,res)=>{
-    res.sendFile(__dirname + '/views/productCar.html');
+app.get("/login",(req,res)=> {
+    let login = path.join(__dirname + '/views/login.html');
+    res.sendFile(login);
 });
 
-app.get('/productDetail', (req,res)=>{
-    res.sendFile(__dirname + '/views/productDetail.html');
+app.get("/register",(req,res)=> {
+    let register = path.join(__dirname + '/views/register.html');
+    res.sendFile(register);
 });
 
-app.get('/register', (req,res)=>{   
-    res.sendFile(__dirname + '/views/register.html');
-});
-
-app.get('/miCuenta', (req,res)=>{   
-    res.sendFile(__dirname + '/views/panelUsuario.html');
-});
-
-app.get('/nosotros', (req,res)=>{   
-    res.sendFile(__dirname + '/views/nosotros.html');
+app.get("/nosotros",(req,res)=> {
+    let nosotros = path.join(__dirname + '/views/nosotros.html');
+    res.sendFile(nosotros);
 });
 
