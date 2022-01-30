fe516edc1f314700987f5115ae4600bd7f40b8b4
noegal97 <noeliagale97@gmail.com>
2022-01-30 09:31:45 -0300

    rutas creadas, falta coordinar modificaciones que dan error en html

--- app.js
+++ app.js
@@ -3,37 +3,13 @@ const app = express();
 const path = require("path");
 
 
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), ()=> console.log('On http://localhost:'+app.get('port')));
 app.use('/static', express.static(__dirname + '/public'));
 
-app.set('port', process.env.PORT || 3000) 
 
 
 
-app.listen(app.get('port'), ()=> console.log('On http://localhost:'+app.get('port')));
-
-app.get("/",(req,res)=> {
-    let home = path.join(__dirname + '/views/home.html');
-    res.sendFile(home);
-})
-
-
-app.get("/miCuenta",(req,res)=> {
-    let miCuenta = path.join(__dirname + '/views/panelUsuario.html');
-    res.sendFile(miCuenta);
-});
-
-app.get("/login",(req,res)=> {
-    let login = path.join(__dirname + '/views/login.html');
-    res.sendFile(login);
-});
-
-app.get("/register",(req,res)=> {
-    let register = path.join(__dirname + '/views/register.html');
-    res.sendFile(register);
-});
-
-app.get("/nosotros",(req,res)=> {
-    let nosotros = path.join(__dirname + '/views/nosotros.html');
-    res.sendFile(nosotros);
-});
-
+app.use(require('./routes/main'));
+//app.use('/productos',require('./routes/Productos'));
+app.use('/miCuenta',require('./routes/miCuenta'));
---
+++ as
@@ -0,0 +1,24 @@
+
+const express = require('express');
+let router = express.Router();
+
+
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req,res) => {
+});
+router.get('/:genero', (req,res) => {
+});
+router.get('/crear', (req,res) => {
+});
+
+module.exports = router;
+
+
+
+const express = require('express');
+const app = express();
+
+let rutasSeries = require('./routes/series');
