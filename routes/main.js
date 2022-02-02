95253f0ff764d5d3291431651d786bae182baa3d
Davagulez <lezcadav@gmail.com>
2022-02-02 19:33:00 -0300

    home armado y algunos cambios

--- routes/main.js
+++ routes/main.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const path = require("path");
 router.get("/",(req,res)=> res.sendFile(path.join(__dirname,'../views/home.html')));
 router.get("/nosotros",(req,res)=> res.sendFile(path.join(__dirname,'../views/nosotros.html')));
-
+router.get("/productDetail",(req,res)=> res.sendFile(path.join(__dirname,'../views/producDetail.html')));
 module.exports = router;
