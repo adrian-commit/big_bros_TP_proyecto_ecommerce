a740c165e31d9332a26d41dcac63ff1b7749c7b4
Davagulez <davagulez@hotmail.es>
2022-05-07 23:33:03 -0300

    cambios en la información de las seeds

--- src/database/seeds/20220502231350-Products.js
+++ src/database/seeds/20220502231350-Products.js
@@ -6,7 +6,7 @@ module.exports = {
    await queryInterface.bulkInsert('products', [
      {
       id: 1,
-      name: 'Alfajores',
+      name: 'Alfajores de chocolate x12',
       description: 'Descripción de alfajores',
       price: 600,
       amount: 12,
@@ -16,7 +16,7 @@ module.exports = {
      },
      {
       id: 2,
-      name: "Cookies",
+      name: "Cookies x12",
       description: "Cookies",
       price: 400,
       amount: 12,
@@ -26,7 +26,7 @@ module.exports = {
      },
      {
       id: 3,
-      name: "Pan Dulce",
+      name: "Pan Dulce 1",
       description: "Pan dulce",
       price: 1200,
       amount: 1,
@@ -36,7 +36,7 @@ module.exports = {
      },
      {
       id: 4,
-      name: "Budines",
+      name: "Budin 1",
       description: "Budines",
       price: 900,
       amount: 1,
@@ -44,6 +44,46 @@ module.exports = {
       category: 2,
       image: 4
      },
+     {
+      id: 5,
+      name: 'Alfajores de chocolate x6',
+      description: 'Descripción de alfajores',
+      price: 300,
+      amount: 6,
+      stock: 30,
+      category: 1,
+      image: 5
+     },
+     {
+      id: 6,
+      name: "Cookies x6",
+      description: "Cookies",
+      price: 200,
+      amount: 6,
+      stock: 50,
+      category: 4,
+      image: 6
+     },
+     {
+      id: 7,
+      name: "Alfacoockies x12",
+      description: "Cookies",
+      price: 800,
+      amount: 12,
+      stock: 20,
+      category: 1,
+      image: 7
+    },
+    {
+      id: 8,
+      name: "Alfacoockies x6",
+      description: "Cookies",
+      price: 400,
+      amount: 6,
+      stock: 10,
+      category: 1,
+      image: 8
+    }
     ], {});
     
   },
